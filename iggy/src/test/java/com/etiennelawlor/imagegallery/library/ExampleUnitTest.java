b3be94a12ef6283183884dac6634896d7b51fa46
ayo <arezrod_16@hotmail.com>
2016-11-19 12:39:54 +0100

    -Added PaletteHelper to easily apply palette into the full image activity

---
+++ iggy/src/test/java/com/etiennelawlor/imagegallery/library/ExampleUnitTest.java
@@ -0,0 +1,2 @@
+package com.etiennelawlor.imagegallery.library;
+
--- sample/src/main/java/com/etiennelawlor/imagegallery/activities/MainActivity.java
+++ sample/src/main/java/com/etiennelawlor/imagegallery/activities/MainActivity.java
@@ -3,15 +3,21 @@ package com.etiennelawlor.imagegallery.activities;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
+import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.text.TextUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.etiennelawlor.imagegallery.R;
 import com.etiennelawlor.imagegallery.library.ImageGalleryFragment;
+import com.etiennelawlor.imagegallery.library.PaletteHelper;
 import com.etiennelawlor.imagegallery.library.activities.FullScreenImageGalleryActivity;
 import com.etiennelawlor.imagegallery.library.activities.ImageGalleryActivity;
 import com.etiennelawlor.imagegallery.library.adapters.FullScreenImageGalleryAdapter;
@@ -19,9 +25,14 @@ import com.etiennelawlor.imagegallery.library.adapters.ImageGalleryAdapter;
 import com.etiennelawlor.imagegallery.library.enums.PaletteColorType;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -30,10 +41,6 @@ import butterknife.OnClick;
  * Created by etiennelawlor on 8/20/15.
  */
 public class MainActivity extends AppCompatActivity implements ImageGalleryAdapter.ImageThumbnailLoader, FullScreenImageGalleryAdapter.FullScreenImageLoader {
-
-    // region Member Variables
-    private PaletteColorType paletteColorType;
-    // endregion
 
     // region Listeners
     @OnClick(R.id.image_gallery_activity_btn)
@@ -74,9 +81,6 @@ public class MainActivity extends AppCompatActivity implements ImageGalleryAdapt
         ImageGalleryActivity.setImageThumbnailLoader(this);
         ImageGalleryFragment.setImageThumbnailLoader(this);
         FullScreenImageGalleryActivity.setFullScreenImageLoader(this);
-
-        // optionally set background color using Palette for full screen images
-        paletteColorType = PaletteColorType.VIBRANT;
     }
     // endregion
 
@@ -108,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements ImageGalleryAdapt
                             Bitmap bitmap = ((BitmapDrawable) iv.getDrawable()).getBitmap();
                             Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
                                 public void onGenerated(Palette palette) {
-                                    applyPalette(palette, bgLinearLayout);
+                                    PaletteHelper.applyPalette(palette, PaletteColorType.VIBRANT, bgLinearLayout);
                                 }
                             });
                         }
@@ -122,124 +126,20 @@ public class MainActivity extends AppCompatActivity implements ImageGalleryAdapt
             iv.setImageDrawable(null);
         }
     }
-    // endregion
 
-    // region Helper Methods
-    private void applyPalette(Palette palette, LinearLayout bgLinearLayout){
-        int bgColor = getBackgroundColor(palette);
-        if (bgColor != -1)
-            bgLinearLayout.setBackgroundColor(bgColor);
+    @Override
+    public void imageShareOption(Uri image) {
+        Toast.makeText(this, "Share this", Toast.LENGTH_LONG).show();
     }
 
-    private int getBackgroundColor(Palette palette) {
-        int bgColor = -1;
+    String imgUrl;
 
-        int vibrantColor = palette.getVibrantColor(0x000000);
-        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
-        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
+    @Override
+    public void imageDownloadOption(String imageUrl) {
+        Toast.makeText(this, "Download this", Toast.LENGTH_LONG).show();
 
-        int mutedColor = palette.getMutedColor(0x000000);
-        int lightMutedColor = palette.getLightMutedColor(0x000000);
-        int darkMutedColor = palette.getDarkMutedColor(0x000000);
-
-        if (paletteColorType != null) {
-            switch (paletteColorType) {
-                case VIBRANT:
-                    if (vibrantColor != 0) { // primary option
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) { // fallback options
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case LIGHT_VIBRANT:
-                    if (lightVibrantColor != 0) { // primary option
-                        bgColor = lightVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case DARK_VIBRANT:
-                    if (darkVibrantColor != 0) { // primary option
-                        bgColor = darkVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case MUTED:
-                    if (mutedColor != 0) { // primary option
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) { // fallback options
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case LIGHT_MUTED:
-                    if (lightMutedColor != 0) { // primary option
-                        bgColor = lightMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case DARK_MUTED:
-                    if (darkMutedColor != 0) { // primary option
-                        bgColor = darkMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        return bgColor;
+        imgUrl = imageUrl;
     }
     // endregion
+
 }
